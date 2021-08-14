6b4a69c1b56e2ee013a79a42d728ad801baf2f1d
mkhlzhkv <mihailzhukov17@gmail.com>
2021-08-14 16:05:40 +0300

    Добавлен тест проверки названия и цены товара при добавлении в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -8,4 +8,11 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main p.price_color")
+    PRODUCT_NAME_TO_BASKET = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRODUCT_PRICE_TO_BASKET = (By.CSS_SELECTOR, "div.alertinner p strong")
+
 
